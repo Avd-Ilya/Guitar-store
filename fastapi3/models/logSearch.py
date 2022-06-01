b9961410f844bf55e5749e73537f122665d1cb9f
Sanana Mamedova <mamedova.sanana@mail.ru>
2022-06-01 23:10:49 +0300

    update

--- fastapi3/models/logSearch.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-import datetime
-
-# популярные поисковые запросы пользователей
-class Search(BaseModel):
-    IP_address: str
-    product: str
-    dateT: datetime.datetime
